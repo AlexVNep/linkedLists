037b63bd5aff804a48cef317b0008feff45e501e
Alex <alexvnep@gmail.com>
2024-10-11 11:09:14 +0200

    Complete initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-      title: "",
+      title: "Linked Lists",
       inject: "body",
     }),
   ],
